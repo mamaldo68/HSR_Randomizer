a65cfa86ee0e8e5db2313fa874450fa7aca7af03
mamaldo <mamaldo68@gmail.com>
2023-11-01 16:08:14 -0400

    Add message generator

--- script.js
+++ script.js
@@ -9,7 +9,21 @@ const debuffer = ["Silver wolf", "Pela", "Topaz & Numby", "Guinaifen", "Luka", "
 const amplifier = ["Bronya", "Tingyun", "Asta", "Yukong"];
 const sustain = ["Fu Xuan", "Luocha", "Gepard", "Lynx", "Bailu", "March 7th", "Natasha", "Trailblazer"];
 
+// returns a random index from a given array
 const randomizer = (arr) => {
-    return Math.floor(Math.random() * arr.length);
+    return arr[Math.floor(Math.random() * arr.length)];
 };
 
+// creates the message from given arrays
+const messageGenerator = (path, damage, debuff, amp, sustain) => {
+    console.log('Challenge yourself in the Simulated Universe!');
+    console.log(`Pick the path of ${randomizer(path)}`);
+    console.log('Your team composition should be:');
+    console.log(`${randomizer(damage)} as your main damage dealer`);
+    console.log(`${randomizer(debuff)} as your debuffer`);
+    console.log(`${randomizer(amp)} as your amplifier`);
+    console.log(`${randomizer(sustain)} as your sustain`);
+    console.log('Good luck!');
+};
+
+messageGenerator(path, damageDealer, debuffer, amplifier, sustain);
